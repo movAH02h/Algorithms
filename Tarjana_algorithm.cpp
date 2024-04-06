3112741ae2e5a1701f2783a6c23a897bcfc0e617
Kirill Rumyantsev <KARum2004@yandex.ru>
2024-04-06 12:27:29 +0300

    algorithm Tarjana to search SCC

---
+++ Tarjana_algorithm.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+int id = 0;
+int countSCC = 0;
+
+void dfs(int vertex, std::vector<std::vector<int>> edges, std::vector<int>& ids, std::vector<int>& lows, std::vector<bool>& onStack, std::stack<int>& st)
+{
+    st.push(vertex);
+    onStack[vertex] = true;
+
+    ids[vertex] = lows[vertex] = id++;
+
+    for (int element : edges[vertex])
+    {
+        if (ids[element] == -1)
+        {
+            dfs(element, edges, ids, lows, onStack, st);
+        }
+        if (onStack[element])
+        {
+            lows[vertex] = std::min(lows[vertex], lows[element]);
+        }
+    }
+
+    if (ids[vertex] == lows[vertex])
+    {
+        while (true)
+        {
+            int node = st.top();
+            st.pop();
+            onStack[node] = false;
+            lows[node] = ids[vertex];
+            if (node == vertex) break;
+        }
+        countSCC++;
+    }
+}
+
+int main()
+{
+    std::cout << "Enter a number of vertex:\n";
+    int v;
+    std::cin >> v;
+    std::cout << "Enter a number of edges\n";
+    int e;
+    std::cin >> e;
+    std::vector<std::vector<int>> edges(e);
+
+    for (int i = 0; i < e; ++i)
+    {
+        int in, to;
+        std::cin >> in >> to;
+        edges[in - 1].push_back(to - 1);
+    }
+
+    std::vector<int> ids(v, -1); // для записи идентификатора каждого узла
+
+    // для записи наименьшего индекса (чтобы можно было понять, что вершины к одной SCC относятся)
+    std::vector<int> lows(v);
+    std::vector<bool> onStack(v, false);
+    std::stack<int> st;
+
+    for (int i = 0; i < v; ++i)
+    {
+        if (ids[i] == -1)
+        {
+            dfs(i, edges, ids, lows, onStack, st);
+        }
+    }
+
+
+    for (int element : lows)
+    {
+        std::cout << element << " ";
+    }
+    return 0;
+}
+/*
+Test:
+8
+13
+1 2
+2 1
+1 3
+2 4
+3 4
+5 3
+4 5
+4 6
+5 6
+5 7
+7 6
+6 8
+8 7
+*/
+
